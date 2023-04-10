119a311a2f34e45d3e11c7953a331e05907eeb82
hamza harrass <hamzaharrass05@gmail.com>
2023-04-10 10:31:03 +0000

    afficher les image sur dashbord and create table role and table music and fixed problem d'afficher les role in localhost (file:config/database.php-> change -> engine:NULL to InnoDB

--- spotify/database/migrations/2023_04_10_094930_create_music_table.php
+++ spotify/database/migrations/2023_04_10_094930_create_music_table.php
@@ -11,15 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('music', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedBigInteger('role_id')->default(2);
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
-            $table->rememberToken();
+            $table->string('artist');
+            $table->string('category');
+            $table->string('image');
+            $table->string('audio');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('music');
     }
 };
--- spotify/resources/views/dashboard.blade.php
+++ spotify/resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous"/>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
         <link rel="stylesheet" href="/style/style.css">
-        <link rel="shortcut icon" href="image/icons/spotify.png" type="image/x-icon">
+        <link rel="shortcut icon" href="images/icons/spotify.png" type="image/x-icon">
     </head>
     <body class="overflow-hidden bg-black">
         <div class="container-fluid d-flex px-0">
@@ -18,7 +18,7 @@
                     <!-- Logo -->
                     <a href="home.html" id="logo" class="d-flex align-items-center py-3 pl-3 disappear">
                         <!-- <img src="img/logo.svg"  alt="Spotify logo"> -->
-                        <img class="logo" src="/image/icons/spotify.png" alt="">
+                        <img class="logo" src="/images/icons/spotify.png" alt="">
                         <span class="ml-2 text-white">Spotify</span>
                     </a>
                     <!-- /Logo -->
@@ -31,14 +31,14 @@
                     <ul id="sidebar_options" class="px-0 disappear">
                         <li class="position-relative pl-3 mb-2">
                             <a class="d-flex align-items-center" href="#">
-                                <img id="home" src="/image/icons/home.svg" alt="Icon depicting a house">
+                                <img id="home" src="/images/icons/home.svg" alt="Icon depicting a house">
                                 <span class="ml-2 text-white">Home</span>
                             </a>
                         </li>
 
                         <li class="pl-3 mb-2">
                             <a class="d-flex align-items-center" href="#">
-                                <img src="/image/icons/search.svg" alt="Icon depicting a magnifying glass">
+                                <img src="/images/icons/search.svg" alt="Icon depicting a magnifying glass">
                                 <span class="ml-2">Search</span>
                             </a>
                         </li>
@@ -63,7 +63,7 @@
                 <div id="profile" class="d-flex flex-column disappear">
                     <div id="user" class="d-flex align-items-center mt-3 mb-2 pl-3">
                         <div class="user_img d-flex justify-content-center align-items-center bg-grey rounded-circle"></div>
-                        <div id="profile_name" class="text-white ml-3">Hamza Harrass</div>
+                        <div id="profile_name" class="text-white ml-3">{{ Auth::user()->name }}</div>
                     </div>
                 </div>
                 <!-- /Profile  -->
@@ -71,15 +71,14 @@
 
             <!-- App content -->
             <main class="d-flex flex-column w-100 px-0 text-grey">
-                <!-- upgrade bar -->
-                <div id="upgrade_bar" class="d-flex justify-content-end align-items-center fixed-top bg-fading-black">
+                <div style="margin-right: 2%" id="upgrade_bar" class="d-flex justify-content-end align-items-center fixed-top mt-3">
                     {{-- <a href="#" class="btn rounded-pill border border-white mr-5 px-5 bg-black text-white text-uppercase">Profile</a> --}}
-                    <div class="dropdown">
+                    <div class="dropdown ">
                         <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
                             {{ Auth::user()->name }}
                         </button>
                         <div class="dropdown-menu">
-                          <a class="dropdown-item" :href="route('profile.edit')">Profil</a>
+                          <a style="cursor: pointer" class="dropdown-item" :href="route('profile.edit')">Profil</a>
                           <form method="POST" action="{{ route('logout') }}">
                             @csrf
                            <button class="dropdown-item" type="submit">Logout</button>
@@ -104,7 +103,7 @@
                     </li>
                 </ul>
 
-                <!-- /Artists -->
+                <!-- Playlist -->
                 <div id="recent" class="d-flex flex-column pl-3 pr-1">
                     <h1 class="pl-2 text-white">Playlist</h1>
                     <!-- <input class="w-25 r" type="text" name="" id=""> -->
@@ -112,7 +111,7 @@
                         <div class="row w-100">
                             <div class="playlist col-6 col-md-3 col-lg-2 d-flex flex-column mb-3 px-2">
                                 <a class="position-relative" href="#">
-                                    <img class="img-fluid" src="/image/7liwa.jpg" alt="">
+                                    <img class="img-fluid" src="/images/7liwa.jpg" alt="">
                                     <div class="playlist_overlay position-absolute d-none w-100 h-100 bg-fading-black">
                                         <i class="position-absolute far fa-play-circle"></i>
                                     </div>
@@ -127,7 +126,7 @@
 
                             <div class="playlist col-6 col-md-3 col-lg-2 d-flex flex-column mb-3 px-2">
                                 <a class="position-relative" href="#">
-                                    <img class="img-fluid" src="/image/Flenn.jpg" alt="">
+                                    <img class="img-fluid" src="/images/Flenn.jpg" alt="">
                                     <div class="playlist_overlay position-absolute d-none w-100 h-100 bg-fading-black">
                                         <i class="position-absolute far fa-play-circle"></i>
                                     </div>
@@ -142,7 +141,7 @@
 
                             <div class="playlist col-6 col-md-3 col-lg-2 d-flex flex-column mb-3 px-2">
                                 <a class="position-relative" href="#">
-                                    <img class="img-fluid" src="/image/ElgrandToto.jpg" alt="">
+                                    <img class="img-fluid" src="/images/ElgrandToto.jpg" alt="">
                                     <div class="playlist_overlay position-absolute d-none w-100 h-100 bg-fading-black">
                                         <i class="position-absolute far fa-play-circle"></i>
                                     </div>
@@ -157,7 +156,7 @@
 
                             <div class="playlist col-6 col-md-3 col-lg-2 d-flex flex-column mb-3 px-2">
                                 <a class="position-relative" href="#">
-                                    <img class="img-fluid" src="/image/inkonnu.jpg" alt="">
+                                    <img class="img-fluid" src="/images/inkonnu.jpg" alt="">
                                     <div class="playlist_overlay position-absolute d-none w-100 h-100 bg-fading-black">
                                         <i class="position-absolute far fa-play-circle"></i>
                                     </div>
@@ -172,7 +171,7 @@
 
                             <div class="playlist col-6 col-md-3 col-lg-2 d-flex flex-column mb-3 px-2">
                                 <a class="position-relative" href="#">
-                                    <img class="img-fluid" src="image/eminem.jpg" alt="">
+                                    <img class="img-fluid" src="images/eminem.jpg" alt="">
                                     <div class="playlist_overlay position-absolute d-none w-100 h-100 bg-fading-black">
                                         <i class="position-absolute far fa-play-circle"></i>
                                     </div>
@@ -187,7 +186,7 @@
 
                             <div class="playlist col-6 col-md-3 col-lg-2 d-flex flex-column mb-3 px-2">
                                 <a class="position-relative" href="#">
-                                    <img class="img-fluid" src="/image/lferda.jpg" alt="">
+                                    <img class="img-fluid" src="/images/lferda.jpg" alt="">
                                     <div class="playlist_overlay position-absolute d-none w-100 h-100 bg-fading-black">
                                         <i class="position-absolute far fa-play-circle"></i>
                                     </div>
@@ -202,7 +201,7 @@
 
                             <div class="playlist col-6 col-md-3 col-lg-2 d-flex flex-column mb-3 px-2">
                                 <a class="position-relative" href="#">
-                                    <img class="img-fluid" src="/image/maitre gims.jpg" alt="">
+                                    <img class="img-fluid" src="/images/maitre gims.jpg" alt="">
                                     <div class="playlist_overlay position-absolute d-none w-100 h-100 bg-fading-black">
                                         <i class="position-absolute far fa-play-circle"></i>
                                     </div>
@@ -217,7 +216,7 @@
 
                             <div class="playlist col-6 col-md-3 col-lg-2 d-flex flex-column mb-3 px-2">
                                 <a class="position-relative" href="#">
-                                    <img class="img-fluid" src="/image/stromae.jpg" alt="">
+                                    <img class="img-fluid" src="/images/stromae.jpg" alt="">
                                     <div class="playlist_overlay position-absolute d-none w-100 h-100 bg-fading-black">
                                         <i class="position-absolute far fa-play-circle"></i>
                                     </div>
@@ -232,7 +231,7 @@
 
                             <div class="playlist col-6 col-md-3 col-lg-2 d-flex flex-column mb-3 px-2">
                                 <a class="position-relative" href="#">
-                                    <img class="img-fluid" src="/image/dollypran.jpg" alt="">
+                                    <img class="img-fluid" src="/images/dollypran.jpg" alt="">
                                     <div class="playlist_overlay position-absolute d-none w-100 h-100 bg-fading-black">
                                         <i class="position-absolute far fa-play-circle"></i>
                                     </div>
@@ -247,7 +246,7 @@
 
                             <div class="playlist col-6 col-md-3 col-lg-2 d-flex flex-column mb-3 px-2">
                                 <a class="position-relative" href="#">
-                                    <img class="img-fluid" src="/image/kouz1.jpg" alt="">
+                                    <img class="img-fluid" src="/images/kouz1.jpg" alt="">
                                     <div class="playlist_overlay position-absolute d-none w-100 h-100 bg-fading-black">
                                         <i class="position-absolute far fa-play-circle"></i>
                                     </div>
@@ -262,7 +261,7 @@
 
                             <div class="playlist col-6 col-md-3 col-lg-2 d-flex flex-column mb-3 px-2">
                                 <a class="position-relative" href="#">
-                                    <img class="img-fluid" src="/image/dizzy dros.jpg" alt="">
+                                    <img class="img-fluid" src="/images/dizzy dros.jpg" alt="">
                                     <div class="playlist_overlay position-absolute d-none w-100 h-100 bg-fading-black">
                                         <i class="position-absolute far fa-play-circle"></i>
                                     </div>
@@ -277,7 +276,7 @@
 
                             <div class="playlist col-6 col-md-3 col-lg-2 d-flex flex-column mb-3 px-2">
                                 <a class="position-relative" href="#">
-                                    <img class="img-fluid" src="/image/don bigg.jpg" alt="">
+                                    <img class="img-fluid" src="/images/don bigg.jpg" alt="">
                                     <div class="playlist_overlay position-absolute d-none w-100 h-100 bg-fading-black">
                                         <i class="position-absolute far fa-play-circle"></i>
                                     </div>
