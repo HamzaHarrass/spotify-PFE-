9aaf8156061b51ac4b3ad228c9c7be075c405792
hamza harrass <hamzaharrass05@gmail.com>
2023-04-11 09:18:36 +0000

    ajouter file layout pour stocker les choses qui répéte sur ma sit web est ajouter un class sur style css

--- spotify/resources/views/categories.blade.php
+++ spotify/resources/views/categories.blade.php
@@ -1,77 +1,6 @@
-<!DOCTYPE html>
-<html lang="it">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>My Spotify</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous"/>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
-        <link rel="stylesheet" href="/style/style.css">
-        <link rel="shortcut icon" href="images/icons/spotify.png" type="image/x-icon">
-    </head>
-    <body class="overflow-hidden bg-black">
-        <div class="container-fluid d-flex px-0">
-            <!-- Left sidebar -->
-            <nav id="sidebar" class="d-flex justify-content-between flex-column fixed-left flex-shrink-0 px-0 text-grey bg-black">
-                <div class="wrapper">
-                    <!-- Logo -->
-                    <a href="home.html" id="logo" class="d-flex align-items-center py-3 pl-3 disappear">
-                        <!-- <img src="img/logo.svg"  alt="Spotify logo"> -->
-                        <img class="logo" src="/images/icons/spotify.png" alt="">
-                        <span class="ml-2 text-white">Spotify</span>
-                    </a>
-                    <!-- /Logo -->
+@extends('layout')
 
-                    <a class="d-none appear mb-4" href="/">
-                        <img src="/images/logo-small.svg" alt="Small Spotify logo">
-                    </a>
-
-                    <!-- Sidebar options -->
-                    <ul id="sidebar_options" class="px-0 disappear">
-                        <li class="position-relative pl-3 mb-2">
-                            <a class="d-flex align-items-center" href="#">
-                                <img id="home" src="/images/icons/home.svg" alt="Icon depicting a house">
-                                <span class="ml-2 text-white">Home</span>
-                            </a>
-                        </li>
-                    </ul>
-                    <!-- /Sidebar options -->
-
-                    <!-- Sidebar options  -->
-                    <div class="d-none appear">
-                        <a href="#" class="d-block mb-3">
-                            <img src="/images/home.svg" alt="Icon depicting a house">
-                        </a>
-
-                        <a href="#" class="d-block mb-3">
-                            <img src="/images/search.svg" alt="Icon depicting a magnifying glass">
-                        </a>
-                    </div>
-                    <!-- /Sidebar options -->
-
-                </div>
-
-            </nav>
-
-            <!-- App content -->
-            <main class="d-flex flex-column w-100 px-0 text-grey">
-                <div  style="margin-right: 2%" id="upgrade_bar" class="d-flex justify-content-end  align-items-center fixed-top mt-3">
-                    {{-- <a href="#" class="btn rounded-pill border border-white mr-5 px-5 bg-black text-white text-uppercase">Profile</a> --}}
-                    <div  class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
-                            {{ Auth::user()->name }}
-                        </button>
-                        <div class="dropdown-menu">
-                          <a style="cursor: pointer" class="dropdown-item" href="{{ route('profile.edit') }}">Profil</a>
-                          <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                           <button class="dropdown-item" type="submit">Logout</button>
-                          </form>
-                        </div>
-                      </div>
-                </div>
+@section('content')
 
                 <!-- categories -->
                 <ul id="categorie" class="d-flex justify-content-center flex-wrap px-2 pt-4 mb-0 text-uppercase">
@@ -100,9 +29,8 @@
                                             @csrf
                                         <h5 class="">Categories Title</h5>
                                         <input class="px-4" type="text" name="name" id="">
-                                        <button type="submit">add</button>
                                         <br>
-                                          <input id="AddCate" class="mt-3 btn rounded-pill text-white border border-white mr-5 px-5" type="button" value="Add">
+                                        <button id="category" class="mt-3 btn rounded-pill text-white border border-white mr-5 px-5" type="submit">add</button>
                                         </form>
                                       </div>
                                     </div>
@@ -140,6 +68,4 @@
                         </div>
                     </div>
                 </div>
-            </main>
-    </body>
-</html>
+        @endsection
---
+++ spotify/resources/views/layout.blade.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="/style/style.css">
+        <link rel="shortcut icon" href="images/icons/spotify.png" type="image/x-icon">
+    </head>
+    <body class="overflow-hidden bg-black">
+        <div class="container-fluid d-flex px-0">
+            <!-- Left sidebar -->
+            <nav id="sidebar" class="d-flex justify-content-between flex-column fixed-left flex-shrink-0 px-0 text-grey bg-black">
+                <div class="wrapper">
+                    <!-- Logo -->
+                    <a href="home.html" id="logo" class="d-flex align-items-center py-3 pl-3 disappear">
+                        <!-- <img src="img/logo.svg"  alt="Spotify logo"> -->
+                        <img class="logo" src="/images/icons/spotify.png" alt="">
+                        <span class="ml-2 text-white">Spotify</span>
+                    </a>
+                    <!-- /Logo -->
+
+                    <a class="d-none appear mb-4" href="/">
+                        <img src="/img/logo-small.svg" alt="Small Spotify logo">
+                    </a>
+
+                    <!-- Sidebar options -->
+                    <ul id="sidebar_options" class="px-0 disappear">
+                        <li class="position-relative pl-3 mb-2">
+                            <a class="d-flex align-items-center" href="#">
+                                <img id="home" src="/images/icons/home.svg" alt="Icon depicting a house">
+                                <span class="ml-2 text-white">Home</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- /Sidebar options -->
+
+                    <!-- Sidebar options  -->
+                    <div class="d-none appear">
+                        <a href="#" class="d-block mb-3">
+                            <img src="/images/icons/home.svg" alt="Icon depicting a house">
+                        </a>
+                    </div>
+                    <!-- /Sidebar options -->
+
+                </div>
+
+                <!-- Profile  -->
+                <div id="profile" class="d-flex flex-column disappear">
+                    <div id="user" class="d-flex align-items-center mt-3 mb-2 pl-3">
+                        <div class="user_img d-flex justify-content-center align-items-center bg-success rounded-circle"></div>
+                        <div id="profile_name" class="text-white ml-3">{{ Auth::user()->name }}</div>
+                    </div>
+                </div>
+                <!-- /Profile  -->
+            </nav>
+
+            <!-- App content -->
+            <main class="d-flex flex-column w-100 px-0 text-grey">
+                <div  style="margin-right: 2%" id="upgrade_bar" class="d-flex justify-content-end  align-items-center fixed-top mt-3">
+                    {{-- <a href="#" class="btn rounded-pill border border-white mr-5 px-5 bg-black text-white text-uppercase">Profile</a> --}}
+                    <div  class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </button>
+                        <div class="dropdown-menu">
+                          <a style="cursor: pointer" class="dropdown-item" href="{{ route('profile.edit') }}">Profil</a>
+                          <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                           <button class="dropdown-item" type="submit">Logout</button>
+                          </form>
+                        </div>
+                      </div>
+                </div>
+
+                @yield('content')
+
+
+            <!-- Now-playing bar -->
+            <footer id="now_playing" class="d-flex justify-content-between align-items-center px-sm-3 fixed-bottom bg-dark-grey">
+                <!-- Left section (current song) -->
+                <div id="song_info" class="d-flex align-items-center">
+                    <a href="#">
+                        <img class="img-fluid" src="/image/lferda.jpg" alt="">
+                    </a>
+                    <div class="disappear ml-2 mr-4">
+                        <a href="#" class="d-block text-white">Ma Jolie</a>
+                        <a href="#" id="current_song" class="text-grey">Lferda</a>
+                    </div>
+                    <div class="disappear d-flex flex-wrap">
+                        <a href="#" class="mr-3">
+                            <i class="far fa-heart"></i>
+                        </a>
+
+                    </div>
+                </div>
+
+
+                <!-- Central section (media controls) -->
+                <div class="d-flex flex-column text-white w-50 mx-2 mx-sm-4">
+                    <div id="media_icons" class="d-flex justify-content-center align-items-center mb-3">
+                        <i class="mr-2 mr-sm-4 fas fa-random"></i>
+                        <i class="mr-2 mr-sm-4 fas fa-step-backward"></i>
+                        <i class="mr-2 mr-sm-4 px-2 far fa-play-circle"></i>
+                        <i class="mr-2 mr-sm-4 fas fa-step-forward"></i>
+                        <i class="fas fa-undo"></i>
+                    </div>
+                    <div id="media_bar" class="d-flex align-items-center">
+                        <span class="font-weight-normal">0:25</span>
+                        <div id="progress_bar" class="mx-4 rounded-pill">
+                            <div class="slider position-relative w-25 h-100 rounded-pill">
+                                <div class="slider_handle position-absolute d-none rounded-circle"></div>
+                            </div>
+                        </div>
+                        <span class="font-weight-normal">4:35</span>
+                    </div>
+                </div>
+
+                <!-- Right section (volume and miscellaneous) -->
+                <div id="volume" class="disappear align-items-center d-flex text-white">
+                    <i class="mr-3 fas fa-volume-up"></i>
+                    <div id="volume_bar" class="rounded-pill">
+                        <div class="slider position-relative w-25 h-100 rounded-pill">
+                            <div class="slider_handle position-absolute d-none rounded-circle"></div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+
+        <!-- <script src="/js/app.js"></script> -->
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    </body>
+</html>
